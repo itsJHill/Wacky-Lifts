da3835ba9d3c8077839cd28a68bc166d864fbba1
Jordan Hill <jordanhill2017@gmail.com>
2026-02-10 10:29:51 -0700

    Initial Commit

---
+++ Lift/LiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftApp.swift
+//  Lift
+//
+//  Created by Jordan Hill on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct LiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
